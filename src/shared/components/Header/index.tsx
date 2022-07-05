2c3151bac155b7f30f3bb19471273d3889a586e5
jordanpurinton <jordanomac@gmail.com>
2022-07-05 18:54:04 -0500

    ss nav for header

--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -2,26 +2,19 @@ import { Anchor, Header as MantineHeader, Text, Title } from '@mantine/core';
 import React, { FC } from 'react';
 import styles from '../../styles/Header.module.scss';
 import Avatar from '../Avatar';
-import Link from 'next/link';
 
 export const Header: FC = () => {
   return (
     <MantineHeader className={styles.header} height={60} p="xs">
-      <Link href="/">
-        <Anchor className={styles.headerAnchor}>
-          <Title order={2}>Say Something</Title>
-        </Anchor>
-      </Link>
-      <Link href="/send-history">
-        <Anchor className={styles.headerAnchor}>
-          <Text className={styles.headerLink}>Send History</Text>
-        </Anchor>
-      </Link>
-      <Link href="/view-history">
-        <Anchor className={styles.headerAnchor}>
-          <Text className={styles.headerLink}>View History</Text>
-        </Anchor>
-      </Link>
+      <Anchor className={styles.headerAnchor} href="/">
+        <Title order={2}>Say Something</Title>
+      </Anchor>
+      <Anchor className={styles.headerAnchor} href="/send-history">
+        <Text className={styles.headerLink}>Send History</Text>
+      </Anchor>
+      <Anchor className={styles.headerAnchor} href="/view-history">
+        <Text className={styles.headerLink}>View History</Text>
+      </Anchor>
       <Avatar />
     </MantineHeader>
   );
